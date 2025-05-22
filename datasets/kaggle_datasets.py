84dff906a1d3998bc3c43907ff8e5ee6d5979982
Kenneth Zheng <113259141+kzkennethzheng@users.noreply.github.com>
2025-05-21 20:40:31 -0400

    Potential kaggle datasets

---
+++ datasets/kaggle_datasets.py
@@ -0,0 +1,13 @@
+import kagglehub
+
+datasets = [
+    "azathoth42/myanimelist",
+    "svanoo/myanimelist-dataset",
+    "marlesson/myanimelist-dataset-animes-profiles-reviews"
+    "andreuvallhernndez/myanimelist"
+    "hernan4444/anime-recommendation-database-2020"
+]
+
+path = kagglehub.dataset_download("azathoth42/myanimelist")
+
+print("Path to dataset files:", path)
